f286d149c9ba13622553f6c5043d7838565c93cd
dskatkov <71693448+dskatkov@users.noreply.github.com>
2023-09-29 20:35:28 +0300

    Update k_nearest_neighbor.py

--- homeworks/assignment0_01_knn/k_nearest_neighbor.py
+++ homeworks/assignment0_01_knn/k_nearest_neighbor.py
@@ -75,7 +75,7 @@ class KNearestNeighbor:
                 # not use a loop over dimension, nor use np.linalg.norm().          #
                 #####################################################################
                 # *****START OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
-                temp = X[i]-self.X_train[j]
+                temp = X[i] - self.X_train[j]
                 dists[i,j]= np.sqrt(np.dot)
                 # *****END OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
         return dists
